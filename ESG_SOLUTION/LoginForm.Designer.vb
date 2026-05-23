383c8c2ad0d89fa08aedcd9860e22bebae3eea56
Duladev <duladewin@gmail.com>
2026-05-23 16:04:26 +0530

    Commit All

---
+++ ESG_SOLUTION/LoginForm.Designer.vb
@@ -0,0 +1,313 @@
+﻿Namespace ESG_SOLUTION
+
+    Partial Class LoginForm
+
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        Protected Overrides Sub Dispose(disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        Private Sub InitializeComponent()
+            pnlLeft = New Panel()
+            lblBrand = New Label()
+            lblTagline = New Label()
+            lblCompany = New Label()
+            pnlStatLine = New Panel()
+            pnlRight = New Panel()
+            btnLogin1 = New RoundedButton()
+            lblWelcome = New Label()
+            lblSub = New Label()
+            lblUserLbl = New Label()
+            lblUserIcon = New Label()
+            txtUsername = New TextBox()
+            pnlUserLine = New Panel()
+            lblPassLbl = New Label()
+            lblPassIcon = New Label()
+            txtPassword = New TextBox()
+            pnlPassLine = New Panel()
+            chkShow = New CheckBox()
+            lblError = New Label()
+            lblVersion = New Label()
+            pnlLeft.SuspendLayout()
+            pnlRight.SuspendLayout()
+            SuspendLayout()
+            ' 
+            ' pnlLeft
+            ' 
+            pnlLeft.BackColor = Color.FromArgb(CByte(15), CByte(22), CByte(30))
+            pnlLeft.Controls.Add(lblBrand)
+            pnlLeft.Controls.Add(lblTagline)
+            pnlLeft.Controls.Add(lblCompany)
+            pnlLeft.Controls.Add(pnlStatLine)
+            pnlLeft.Dock = DockStyle.Left
+            pnlLeft.Location = New Point(0, 0)
+            pnlLeft.Name = "pnlLeft"
+            pnlLeft.Size = New Size(360, 560)
+            pnlLeft.TabIndex = 0
+            ' 
+            ' lblBrand
+            ' 
+            lblBrand.BackColor = Color.Transparent
+            lblBrand.Font = New Font("Georgia", 36.0F, FontStyle.Bold)
+            lblBrand.ForeColor = Color.FromArgb(CByte(240), CByte(245), CByte(248))
+            lblBrand.Location = New Point(28, 194)
+            lblBrand.Name = "lblBrand"
+            lblBrand.Size = New Size(280, 58)
+            lblBrand.TabIndex = 1
+            lblBrand.Text = "DCL-ESG"
+            lblBrand.TextAlign = ContentAlignment.MiddleLeft
+            ' 
+            ' lblTagline
+            ' 
+            lblTagline.BackColor = Color.Transparent
+            lblTagline.Font = New Font("Segoe UI", 8.5F)
+            lblTagline.ForeColor = Color.FromArgb(CByte(16), CByte(185), CByte(129))
+            lblTagline.Location = New Point(38, 258)
+            lblTagline.Name = "lblTagline"
+            lblTagline.Size = New Size(290, 18)
+            lblTagline.TabIndex = 2
+            lblTagline.Text = "Environmental  ·  Social  ·  Governance"
+            ' 
+            ' lblCompany
+            ' 
+            lblCompany.BackColor = Color.Transparent
+            lblCompany.Font = New Font("Segoe UI", 9.5F)
+            lblCompany.ForeColor = Color.FromArgb(CByte(100), CByte(116), CByte(139))
+            lblCompany.Location = New Point(38, 280)
+            lblCompany.Name = "lblCompany"
+            lblCompany.Size = New Size(280, 20)
+            lblCompany.TabIndex = 3
+            lblCompany.Text = "Diamond Cutters Ltd"
+            ' 
+            ' pnlStatLine
+            ' 
+            pnlStatLine.BackColor = Color.FromArgb(CByte(35), CByte(255), CByte(255), CByte(255))
+            pnlStatLine.Location = New Point(38, 326)
+            pnlStatLine.Name = "pnlStatLine"
+            pnlStatLine.Size = New Size(270, 1)
+            pnlStatLine.TabIndex = 4
+            ' 
+            ' pnlRight
+            ' 
+            pnlRight.BackColor = Color.FromArgb(CByte(26), CByte(34), CByte(44))
+            pnlRight.Controls.Add(btnLogin1)
+            pnlRight.Controls.Add(lblWelcome)
+            pnlRight.Controls.Add(lblSub)
+            pnlRight.Controls.Add(lblUserLbl)
+            pnlRight.Controls.Add(lblUserIcon)
+            pnlRight.Controls.Add(txtUsername)
+            pnlRight.Controls.Add(pnlUserLine)
+            pnlRight.Controls.Add(lblPassLbl)
+            pnlRight.Controls.Add(lblPassIcon)
+            pnlRight.Controls.Add(txtPassword)
+            pnlRight.Controls.Add(pnlPassLine)
+            pnlRight.Controls.Add(chkShow)
+            pnlRight.Controls.Add(lblError)
+            pnlRight.Controls.Add(lblVersion)
+            pnlRight.Dock = DockStyle.Fill
+            pnlRight.Location = New Point(360, 0)
+            pnlRight.Name = "pnlRight"
+            pnlRight.Size = New Size(520, 560)
+            pnlRight.TabIndex = 1
+            ' 
+            ' btnLogin1
+            ' 
+            btnLogin1.BackColor = Color.Green
+            btnLogin1.CornerRadius = 8
+            btnLogin1.ForeColor = SystemColors.Control
+            btnLogin1.Location = New Point(116, 445)
+            btnLogin1.Name = "btnLogin1"
+            btnLogin1.Size = New Size(199, 23)
+            btnLogin1.TabIndex = 15
+            btnLogin1.Text = "Sign In"
+            btnLogin1.UseVisualStyleBackColor = False
+            ' 
+            ' lblWelcome
+            ' 
+            lblWelcome.BackColor = Color.Transparent
+            lblWelcome.Font = New Font("Georgia", 26.0F, FontStyle.Bold)
+            lblWelcome.ForeColor = Color.FromArgb(CByte(240), CByte(245), CByte(248))
+            lblWelcome.Location = New Point(48, 82)
+            lblWelcome.Name = "lblWelcome"
+            lblWelcome.Size = New Size(200, 48)
+            lblWelcome.TabIndex = 0
+            lblWelcome.Text = "Sign in"
+            ' 
+            ' lblSub
+            ' 
+            lblSub.BackColor = Color.Transparent
+            lblSub.Font = New Font("Segoe UI", 9.5F)
+            lblSub.ForeColor = Color.FromArgb(CByte(100), CByte(116), CByte(139))
+            lblSub.Location = New Point(50, 132)
+            lblSub.Name = "lblSub"
+            lblSub.Size = New Size(380, 18)
+            lblSub.TabIndex = 1
+            lblSub.Text = "Enter your credentials to access the ESG portal"
+            ' 
+            ' lblUserLbl
+            ' 
+            lblUserLbl.BackColor = Color.Transparent
+            lblUserLbl.Font = New Font("Segoe UI", 7.5F, FontStyle.Bold)
+            lblUserLbl.ForeColor = Color.FromArgb(CByte(100), CByte(116), CByte(139))
+            lblUserLbl.Location = New Point(50, 172)
+            lblUserLbl.Name = "lblUserLbl"
+            lblUserLbl.Size = New Size(100, 16)
+            lblUserLbl.TabIndex = 2
+            lblUserLbl.Text = "USERNAME"
+            ' 
+            ' lblUserIcon
+            ' 
+            lblUserIcon.BackColor = Color.Transparent
+            lblUserIcon.Font = New Font("Segoe UI Emoji", 12.0F)
+            lblUserIcon.ForeColor = Color.FromArgb(CByte(71), CByte(85), CByte(105))
+            lblUserIcon.Location = New Point(50, 194)
+            lblUserIcon.Name = "lblUserIcon"
+            lblUserIcon.Size = New Size(30, 34)
+            lblUserIcon.TabIndex = 3
+            lblUserIcon.Text = "👤"
+            lblUserIcon.TextAlign = ContentAlignment.MiddleCenter
+            ' 
+            ' txtUsername
+            ' 
+            txtUsername.BackColor = Color.FromArgb(CByte(36), CByte(46), CByte(58))
+            txtUsername.BorderStyle = BorderStyle.None
+            txtUsername.Font = New Font("Segoe UI", 10.5F)
+            txtUsername.ForeColor = Color.FromArgb(CByte(226), CByte(232), CByte(240))
+            txtUsername.Location = New Point(84, 200)
+            txtUsername.Name = "txtUsername"
+            txtUsername.PlaceholderText = "Enter username"
+            txtUsername.Size = New Size(330, 19)
+            txtUsername.TabIndex = 4
+            ' 
+            ' pnlUserLine
+            ' 
+            pnlUserLine.BackColor = Color.FromArgb(CByte(50), CByte(65), CByte(80))
+            pnlUserLine.Location = New Point(50, 232)
+            pnlUserLine.Name = "pnlUserLine"
+            pnlUserLine.Size = New Size(368, 1)
+            pnlUserLine.TabIndex = 5
+            ' 
+            ' lblPassLbl
+            ' 
+            lblPassLbl.BackColor = Color.Transparent
+            lblPassLbl.Font = New Font("Segoe UI", 7.5F, FontStyle.Bold)
+            lblPassLbl.ForeColor = Color.FromArgb(CByte(100), CByte(116), CByte(139))
+            lblPassLbl.Location = New Point(50, 252)
+            lblPassLbl.Name = "lblPassLbl"
+            lblPassLbl.Size = New Size(100, 16)
+            lblPassLbl.TabIndex = 6
+            lblPassLbl.Text = "PASSWORD"
+            ' 
+            ' lblPassIcon
+            ' 
+            lblPassIcon.BackColor = Color.Transparent
+            lblPassIcon.Font = New Font("Segoe UI Emoji", 12.0F)
+            lblPassIcon.ForeColor = Color.FromArgb(CByte(71), CByte(85), CByte(105))
+            lblPassIcon.Location = New Point(50, 274)
+            lblPassIcon.Name = "lblPassIcon"
+            lblPassIcon.Size = New Size(30, 34)
+            lblPassIcon.TabIndex = 7
+            lblPassIcon.Text = "🔒"
+            lblPassIcon.TextAlign = ContentAlignment.MiddleCenter
+            ' 
+            ' txtPassword
+            ' 
+            txtPassword.BackColor = Color.FromArgb(CByte(36), CByte(46), CByte(58))
+            txtPassword.BorderStyle = BorderStyle.None
+            txtPassword.Font = New Font("Segoe UI", 10.5F)
+            txtPassword.ForeColor = Color.FromArgb(CByte(226), CByte(232), CByte(240))
+            txtPassword.Location = New Point(84, 280)
+            txtPassword.Name = "txtPassword"
+            txtPassword.PasswordChar = "●"c
+            txtPassword.PlaceholderText = "Enter password"
+            txtPassword.Size = New Size(330, 19)
+            txtPassword.TabIndex = 8
+            ' 
+            ' pnlPassLine
+            ' 
+            pnlPassLine.BackColor = Color.FromArgb(CByte(50), CByte(65), CByte(80))
+            pnlPassLine.Location = New Point(50, 312)
+            pnlPassLine.Name = "pnlPassLine"
+            pnlPassLine.Size = New Size(368, 1)
+            pnlPassLine.TabIndex = 9
+            ' 
+            ' chkShow
+            ' 
+            chkShow.BackColor = Color.Transparent
+            chkShow.Cursor = Cursors.Hand
+            chkShow.Font = New Font("Segoe UI", 9.0F)
+            chkShow.ForeColor = Color.FromArgb(CByte(100), CByte(116), CByte(139))
+            chkShow.Location = New Point(50, 324)
+            chkShow.Name = "chkShow"
+            chkShow.Size = New Size(130, 22)
+            chkShow.TabIndex = 10
+            chkShow.Text = "Show password"
+            chkShow.UseVisualStyleBackColor = False
+            ' 
+            ' lblError
+            ' 
+            lblError.BackColor = Color.Transparent
+            lblError.Font = New Font("Segoe UI", 9.0F)
+            lblError.ForeColor = Color.FromArgb(CByte(239), CByte(68), CByte(68))
+            lblError.Location = New Point(48, 387)
+            lblError.Name = "lblError"
+            lblError.Size = New Size(368, 20)
+            lblError.TabIndex = 11
+            ' 
+            ' lblVersion
+            ' 
+            lblVersion.BackColor = Color.Transparent
+            lblVersion.Font = New Font("Segoe UI", 7.5F)
+            lblVersion.ForeColor = Color.FromArgb(CByte(55), CByte(75), CByte(95))
+            lblVersion.Location = New Point(156, 535)
+            lblVersion.Name = "lblVersion"
+            lblVersion.Size = New Size(170, 16)
+            lblVersion.TabIndex = 14
+            lblVersion.Text = "v1.0.0  ·  © 2024 Diamond Cutters Ltd"
+            ' 
+            ' LoginForm
+            ' 
+            BackColor = Color.FromArgb(CByte(18), CByte(24), CByte(32))
+            ClientSize = New Size(880, 560)
+            Controls.Add(pnlRight)
+            Controls.Add(pnlLeft)
+            FormBorderStyle = FormBorderStyle.None
+            MinimumSize = New Size(880, 560)
+            Name = "LoginForm"
+            StartPosition = FormStartPosition.CenterScreen
+            Text = "DCL-ESG | Login"
+            pnlLeft.ResumeLayout(False)
+            pnlRight.ResumeLayout(False)
+            pnlRight.PerformLayout()
+            ResumeLayout(False)
+        End Sub
+
+        ' ── Designer fields ───────────────────────────────────────────────────
+        Private pnlLeft As System.Windows.Forms.Panel
+        Private lblBrand As System.Windows.Forms.Label
+        Private lblTagline As System.Windows.Forms.Label
+        Private lblCompany As System.Windows.Forms.Label
+        Private pnlStatLine As System.Windows.Forms.Panel
+        Private pnlRight As System.Windows.Forms.Panel
+        Private lblWelcome As System.Windows.Forms.Label
+        Private lblSub As System.Windows.Forms.Label
+        Private lblUserLbl As System.Windows.Forms.Label
+        Private lblUserIcon As System.Windows.Forms.Label
+        Private txtUsername As System.Windows.Forms.TextBox
+        Private pnlUserLine As System.Windows.Forms.Panel
+        Private lblPassLbl As System.Windows.Forms.Label
+        Private lblPassIcon As System.Windows.Forms.Label
+        Private txtPassword As System.Windows.Forms.TextBox
+        Private pnlPassLine As System.Windows.Forms.Panel
+        Private chkShow As System.Windows.Forms.CheckBox
+        Private lblError As System.Windows.Forms.Label
+        Private lblVersion As System.Windows.Forms.Label
+        Private btnExit As RoundedButton
+        Friend WithEvents btnLogin1 As RoundedButton
+    End Class
+
+End Namespace
---
+++ ESG_SOLUTION/LoginForm.vb
@@ -0,0 +1,277 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Drawing.Text
+Imports System.Windows.Forms
+
+Namespace ESG_SOLUTION
+
+    Public Class LoginForm
+        Inherits Form
+
+        ' ── Colours ───────────────────────────────────────────────────────────
+        Private ReadOnly Emerald As Color = Color.FromArgb(16, 185, 129)
+        Private ReadOnly EmeraldHov As Color = Color.FromArgb(5, 150, 105)
+        Private ReadOnly BorderColor As Color = Color.FromArgb(50, 65, 80)
+        Private ReadOnly TextMuted As Color = Color.FromArgb(100, 116, 139)
+        Private ReadOnly TextPrimary As Color = Color.FromArgb(240, 245, 248)
+        Private ReadOnly ErrorColor As Color = Color.FromArgb(239, 68, 68)
+
+        ' ── DB connection — update before deployment ──────────────────────────
+        Private Const ConnStr As String =
+            "Server=DCL-ICT-007/DEVELOPER;Database=ESG;Integrated Security=True;"
+
+        ' ═════════════════════════════════════════════════════════════════════
+        Public Sub New()
+            InitializeComponent()
+            WireEvents()
+            SetRoundedCorners(14)
+        End Sub
+
+        ' ═════════════════════════════════════════════════════════════════════
+        '  Wire events (kept out of Designer file)
+        ' ═════════════════════════════════════════════════════════════════════
+        Private Sub WireEvents()
+            ' Focus highlight on input lines
+            AddHandler txtUsername.GotFocus, Sub(s, e) pnlUserLine.BackColor = Emerald
+            AddHandler txtUsername.LostFocus, Sub(s, e) pnlUserLine.BackColor = BorderColor
+            AddHandler txtPassword.GotFocus, Sub(s, e) pnlPassLine.BackColor = Emerald
+            AddHandler txtPassword.LostFocus, Sub(s, e) pnlPassLine.BackColor = BorderColor
+
+            ' Show / hide password
+            'AddHandler chkShow.CheckedChanged, Sub(s, e)
+            'txtPassword.PasswordChar = If(chkShow.Checked, ChrW(0), "●"c)
+            'End Sub
+
+
+
+            ' Dragging (no title bar)
+            AddHandler pnlLeft.MouseDown, AddressOf FormDrag_MouseDown
+            AddHandler pnlRight.MouseDown, AddressOf FormDrag_MouseDown
+            AddHandler lblWelcome.MouseDown, AddressOf FormDrag_MouseDown
+            AddHandler lblSub.MouseDown, AddressOf FormDrag_MouseDown
+        End Sub
+
+        ' ═════════════════════════════════════════════════════════════════════
+        '  Rounded window corners
+        ' ═════════════════════════════════════════════════════════════════════
+        Private Sub SetRoundedCorners(radius As Integer)
+            AddHandler Me.Shown, Sub(s, e)
+                                     Dim d As Integer = radius * 2
+                                     Dim path As New GraphicsPath()
+                                     path.AddArc(0, 0, d, d, 180, 90)
+                                     path.AddArc(Width - d, 0, d, d, 270, 90)
+                                     path.AddArc(Width - d, Height - d, d, d, 0, 90)
+                                     path.AddArc(0, Height - d, d, d, 90, 90)
+                                     path.CloseFigure()
+                                     Me.Region = New Region(path)
+                                 End Sub
+        End Sub
+
+        ' ═════════════════════════════════════════════════════════════════════
+        '  Custom painting
+        ' ═════════════════════════════════════════════════════════════════════
+        Private Sub PnlLeft_Paint(sender As Object, e As PaintEventArgs)
+            Dim g As Graphics = e.Graphics
+            g.SmoothingMode = SmoothingMode.AntiAlias
+
+            ' Hexagon grid pattern
+            Using pen As New Pen(Color.FromArgb(28, 255, 255, 255), 0.6F)
+                Dim s As Integer = 50
+                Dim y As Integer = -s
+                Do While y < pnlLeft.Height + s
+                    Dim x As Integer = -s
+                    Do While x < pnlLeft.Width + s
+                        DrawHex(g, pen, x, y, s)
+                        x += s + 10
+                    Loop
+                    y += CInt(s * 1.5)
+                Loop
+            End Using
+
+            ' Emerald glow — bottom left
+            Using gp As New GraphicsPath()
+                gp.AddEllipse(-60, pnlLeft.Height - 160, 240, 240)
+                Using br As New PathGradientBrush(gp)
+                    br.CenterColor = Color.FromArgb(55, 16, 185, 129)
+                    br.SurroundColors = New Color() {Color.Transparent}
+                    g.FillPath(br, gp)
+                End Using
+            End Using
+
+            ' Vertical separator
+            Using p As New Pen(Color.FromArgb(45, 255, 255, 255), 1.0F)
+                g.DrawLine(p, pnlLeft.Width - 1, 40, pnlLeft.Width - 1, pnlLeft.Height - 40)
+            End Using
+        End Sub
+
+        Private Sub PnlRight_Paint(sender As Object, e As PaintEventArgs)
+            Dim g As Graphics = e.Graphics
+            Using pen As New Pen(Color.FromArgb(12, 255, 255, 255), 1.0F)
+                Dim y As Integer = 0
+                Do While y < pnlRight.Height
+                    g.DrawLine(pen, 0, y, pnlRight.Width, y)
+                    y += 40
+                Loop
+            End Using
+        End Sub
+
+        Private Shared Sub DrawHex(g As Graphics, pen As Pen, cx As Integer, cy As Integer, r As Integer)
+            Dim pts(5) As PointF
+            For i As Integer = 0 To 5
+                Dim a As Double = Math.PI / 3 * i - Math.PI / 6
+                pts(i) = New PointF(cx + r * CSng(Math.Cos(a)),
+                                    cy + r * CSng(Math.Sin(a)))
+            Next
+            g.DrawPolygon(pen, pts)
+        End Sub
+
+        ' ═════════════════════════════════════════════════════════════════════
+        '  Login logic
+        ' ═════════════════════════════════════════════════════════════════════
+
+        Private Function AuthenticateUser(username As String, password As String) As Boolean
+            ' Matches Users table: Username and UserPws columns
+            Const sql As String =
+                "SELECT COUNT(1) FROM vw_UserLogin WHERE Username = @user AND UserPws = @pass"
+
+            Using conn As New SqlConnection(ConnStr)
+                conn.Open()
+                Using cmd As New SqlCommand(sql, conn)
+                    cmd.Parameters.Add("@user", SqlDbType.NVarChar, 100).Value = username
+                    cmd.Parameters.Add("@pass", SqlDbType.NVarChar, 256).Value = password
+                    Return Convert.ToInt32(cmd.ExecuteScalar()) > 0
+                End Using
+            End Using
+        End Function
+
+        Private Sub ShowError(msg As String)
+            lblError.Text = "⚠  " & msg
+            ShakeAsync(pnlRight, 6)
+        End Sub
+
+        Private Async Sub ShakeAsync(ctrl As Control, amount As Integer)
+            Dim origin As Point = ctrl.Location
+            For i As Integer = 0 To 5
+                ctrl.Left = origin.X + If(i Mod 2 = 0, amount, -amount)
+                Await System.Threading.Tasks.Task.Delay(28)
+            Next
+            ctrl.Left = origin.X
+        End Sub
+
+        ' ═════════════════════════════════════════════════════════════════════
+        '  Drag to move (borderless form)
+        ' ═════════════════════════════════════════════════════════════════════
+        Private _dragStart As Point
+
+        Private Sub FormDrag_MouseDown(sender As Object, e As MouseEventArgs)
+            If e.Button = MouseButtons.Left Then
+                _dragStart = e.Location
+            End If
+        End Sub
+
+        Protected Overrides Sub OnMouseMove(e As MouseEventArgs)
+            If e.Button = MouseButtons.Left Then
+                Location = New Point(Left + e.X - _dragStart.X,
+                                     Top + e.Y - _dragStart.Y)
+            End If
+            MyBase.OnMouseMove(e)
+        End Sub
+
+        Private Sub btnLogin1_Click(sender As Object, e As EventArgs) Handles btnLogin1.Click
+            lblError.Text = ""
+            Dim user As String = txtUsername.Text.Trim()
+            Dim pass As String = txtPassword.Text
+
+            If String.IsNullOrEmpty(user) OrElse String.IsNullOrEmpty(pass) Then
+                ShowError("Please enter both username and password.")
+                Return
+            End If
+
+            btnLogin1.Enabled = True
+            btnLogin1.Text = "Signing in..."
+
+            Try
+                If AuthenticateUser(user, pass) Then
+                    ' TODO: open main dashboard
+                    ' Dim main As New MainForm(user)
+                    ' main.Show()
+                    ' Me.Hide()
+                    MessageBox.Show(
+                        $"Welcome, {user}!" & Environment.NewLine & "Login successful.",
+                        "DCL-ESG",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.None)
+                Else
+                    ShowError("Invalid username or password.")
+                    txtPassword.Clear()
+                    txtUsername.Focus()
+                End If
+            Catch sqlEx As SqlException
+                ShowError("Database error: " & sqlEx.Message)
+            Catch ex As Exception
+                ShowError("Connection error: " & ex.Message)
+            Finally
+                btnLogin1.Enabled = True
+                btnLogin1.Text = "SIGN  IN"
+            End Try
+        End Sub
+
+
+
+        'Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Me.Close()
+        'End Sub
+    End Class
+
+    ' ═════════════════════════════════════════════════════════════════════════
+    '  RoundedButton — shared helper used by Designer
+    ' ═════════════════════════════════════════════════════════════════════════
+    Public Class RoundedButton
+        Inherits Button
+
+        Public Property CornerRadius As Integer = 8
+
+        Protected Overrides Sub OnPaint(e As PaintEventArgs)
+            Dim g As Graphics = e.Graphics
+            g.SmoothingMode = SmoothingMode.AntiAlias
+            g.TextRenderingHint = TextRenderingHint.ClearTypeGridFit
+
+            Dim rect As New Rectangle(0, 0, Width - 1, Height - 1)
+            Using path As GraphicsPath = RoundPath(rect, CornerRadius)
+                Using fill As New SolidBrush(BackColor)
+                    g.FillPath(fill, path)
+                End Using
+
+                If FlatAppearance.BorderSize > 0 Then
+                    Using bp As New Pen(FlatAppearance.BorderColor, 1.0F)
+                        g.DrawPath(bp, path)
+                    End Using
+                End If
+
+                Dim sf As New StringFormat With {
+                    .Alignment = StringAlignment.Center,
+                    .LineAlignment = StringAlignment.Center
+                }
+                Using tb As New SolidBrush(ForeColor)
+                    g.DrawString(Text, Font, tb, rect, sf)
+                End Using
+            End Using
+        End Sub
+
+        Private Shared Function RoundPath(r As Rectangle, rad As Integer) As GraphicsPath
+            Dim d As Integer = rad * 2
+            Dim p As New GraphicsPath()
+            p.AddArc(r.X, r.Y, d, d, 180, 90)
+            p.AddArc(r.Right - d, r.Y, d, d, 270, 90)
+            p.AddArc(r.Right - d, r.Bottom - d, d, d, 0, 90)
+            p.AddArc(r.X, r.Bottom - d, d, d, 90, 90)
+            p.CloseFigure()
+            Return p
+        End Function
+
+    End Class
+
+End Namespace
